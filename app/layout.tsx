f52ccffdfb5d9116d0c48160da4f8f123b37349c
sangle-zotek8 <sangle@zotek8.com>
2024-01-05 13:29:08 +0700

    change reload

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,36 @@ import "./data-tables-css.css";
 import "./satoshi.css";
 import { useState, useEffect } from "react";
 import Loader from "@/components/common/Loader";
-
+import jwt from 'jsonwebtoken';
+import { useRouter } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
+  const router = useRouter()
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
+  }, []);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const decodedToken: any = jwt.decode(token);
+        if (decodedToken && decodedToken.exp) {
+          const expirationTime = decodedToken.exp * 1000;
+          const currentTime = new Date().getTime();
+          if (currentTime <= expirationTime) {
+            router.push('/home');
+          }
+        }
+      } catch (error) {
+        console.error('Error decoding or checking token expiration:', error);
+      }
+    } 
   }, []);
 
   return (
